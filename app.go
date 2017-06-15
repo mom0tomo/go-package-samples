670566fa410aff7950c34f34b9eef10bc2da8c47
momo <mo-watanabe@dip-net.co.jp>
2017-06-15 14:13:32 +0900

    add app.go

---
+++ app.go
@@ -0,0 +1,5 @@
+package main
+
+func AppName() string {
+	return "This is the Rodentia Application!!"
+}
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/mom0tomo/go-package-samples/rodentia"
 )
 
 func main() {
+	fmt.Println(AppName())
+
 	fmt.Println(rodentia.GopherLike())
 	fmt.Println(rodentia.BeaverLike())
 	fmt.Println(rodentia.HamsterLike())
